9e88159d2404720222c5c18570956e3304571876
suganthsugi <suganthjayanthi@gmail.com>
2023-03-06 01:18:03 +0530

    added response module

---
+++ models/Response.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const ResponseSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    qnPaper: {
+        type: mongoose.Schema.Types.ObjectId,
+        Ref: 'QnPaper',
+        required: true
+    },
+    choice: {
+        type: [Number],
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    attemptNumber: {
+        type: Number,
+        required: true,
+        default: 1,
+        unique: true
+    },
+    timeTaken: {
+        type: Number,
+        required: true
+    },
+    feedback: {
+        type: String,
+        required: true,
+        default: "---___---___---"
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Response', ResponseSchema);

faeacc4b04dfbf8c2b44a6cd89dc8b98c5f470fb
suganthsugi <suganthjayanthi@gmail.com>
2023-03-07 15:33:49 +0530

    sydbhuj

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    // res.header()
     next();
   });
   

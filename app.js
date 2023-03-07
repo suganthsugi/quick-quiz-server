70a4ef183b0dc4f6f9bdea92b709d92a978313ee
suganthsugi <suganthjayanthi@gmail.com>
2023-03-07 15:42:10 +0530

    iuawetyu

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ app.use((req, res, next) => {
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     // res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', 'https://quickquiz-bc991.firebaseapp.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', 'https://quickquiz-bc991.firebaseapp.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     // res.header()
     next();
   });

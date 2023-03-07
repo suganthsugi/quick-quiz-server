3f1635ce03e19c7fcc1687fceb52e17756279670
suganthsugi <suganthjayanthi@gmail.com>
2023-03-07 13:41:02 +0530

    h

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cors());
 app.use(cookieParser());
 app.use(function (req, res, next) {
-    const allowedOrigins = ['*'];
+    const allowedOrigins = ['http://127.0.0.1:3000'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);

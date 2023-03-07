bdbbaf97ae4e3b13170711fc39f83f48fe5955b4
suganthsugi <suganthjayanthi@gmail.com>
2023-03-07 15:46:32 +0530

    wiuert

--- app.js
+++ app.js
@@ -24,29 +24,29 @@ app.use(cookieParser());
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 // });
-// app.use(function (req, res, next) {
-//     const allowedOrigins = ['http://localhost:3000', 'https://quickquiz-bc991.firebaseapp.com']; // update with your own domain
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//         res.setHeader('Access-Control-Allow-Origin', origin);
-//     }
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-//     next();
-// });
-app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    // res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', 'https://quickquiz-bc991.firebaseapp.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+app.use(function (req, res, next) {
+    const allowedOrigins = ['http://localhost:3000', 'https://quickquiz-bc991.firebaseapp.com']; // update with your own domain
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    // res.header()
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
     next();
-  });
+});
+// app.use((req, res, next) => {
+//     // res.setHeader('Access-Control-Allow-Origin', '*');
+//     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     // res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.setHeader('Access-Control-Allow-Origin', 'https://quickquiz-bc991.firebaseapp.com');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     // res.header()
+//     next();
+//   });
   
   
 

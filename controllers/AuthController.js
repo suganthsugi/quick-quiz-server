5b7655fe8fcacf67b3f33151114ad344092a9dae
suganthsugi <suganthjayanthi@gmail.com>
2023-03-08 14:55:10 +0530

    expire 400

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -311,9 +311,9 @@ exports.login = async (req, res) => {
             const jwt_token = jwt.sign({ user_id: currUser._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
             // res.cookie('myCookie', 'someValue', { maxAge: 900000 });
             const cookieOptions = {
-                expires: new Date(
-                  Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-                ), //after th data we mentioned the client will delete it
+                // expires: new Date(
+                //   Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+                // ), //after th data we mentioned the client will delete it
                 // secure: true, //by setting this cookie will only be sent over encryped connection
                 httpOnly: true, //cookie cannnot be accessed or modified in any way by the browser
               };

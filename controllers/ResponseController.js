fb75e2404f7afaaa0d2e628350b42a2354f62688
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-04-12 15:30:55 +0530

    0.2

--- controllers/ResponseController.js
+++ controllers/ResponseController.js
@@ -4,25 +4,27 @@ const Response=require("../models/Response");
 exports.submitResponse=async(req,res)=>{
     try
     {
-    const {answerScript,uid,qid}=req.body;
+    const {answerscript,uid,qid}=req.body;
     const qset=await QuestionPaper.findById(qid);
     const eachQuestion=qset.questions;
     var score=0;
     var totalScore=0;
     var correctCount=0; 
-    // eachQuestion.forEach(data => {
-    //     if(answerScript[data.question]==data.correctAnswer)
-    //     {
-    //        score=score+data.mark;
-    //        correctCount+=1;
-    //     } 
-    //     totalScore+=data.mark;
-    // });
+    console.log("working");
+    eachQuestion.forEach(data => {
+        if(answerscript[data.question]==data.correctAnswer)
+        {
+           score=score+data.mark;
+           correctCount+=1;
+        }
+        console.log(data); 
+        totalScore+=data.mark;
+    });
     res.json({
         score,
         totalScore,
         correctCount,
-        totalCount:eachQuestion.length()
+        totalCount:eachQuestion.length
     })
     }
     catch(err)

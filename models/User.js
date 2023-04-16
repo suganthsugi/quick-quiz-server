0316b277637b3a067bfc25d2bf14aac5017b261f
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-04-16 15:26:24 +0530

    0.2

--- models/User.js
+++ models/User.js
@@ -1,5 +1,86 @@
 const mongoose = require('mongoose');
-
+const practiceSchema=new mongoose.Schema({
+    qnPaper: {
+        type: mongoose.Schema.Types.ObjectId,
+        Ref: 'QnPaper',
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    totalquestions: {
+        type: Number,
+        required: true
+    },
+    correctanswers: {
+        type: Number,
+        required: true
+    },
+    totalscore: {
+        type: Number,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    timeTaken: {
+        type: Number,
+        required: true,
+        default:"0"
+    }
+}, { timestamps: true })
+const testSchema=new mongoose.Schema({
+    qnPaper: {
+        type: mongoose.Schema.Types.ObjectId,
+        Ref: 'QnPaper',
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    totalquestions: {
+        type: Number,
+        required: true
+    },
+    correctanswers: {
+        type: Number,
+        required: true
+    },
+    totalscore: {
+        type: Number,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    timeTaken: {
+        type: Number,
+        required: true,
+        default:"0"
+    }
+}, { timestamps: true })
+const statsSchema = new mongoose.Schema({
+    easy: {
+      type: Number,
+      default: 0,
+    },
+    medium: {
+      type: Number,
+      default: 0,
+    },
+    hard: {
+      type: Number,
+      default: 0,
+    },
+    rating:{
+        type:Number,
+        default:0,
+    }
+  });
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -37,6 +118,11 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    pratice:[practiceSchema],
+    test:[testSchema],
+    stats: {
+        type: statsSchema,
+    }
 }, { timestamps: true })
 
 

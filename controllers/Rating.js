5d3c6e19aad81615bdb10a3e8c98fea710324b76
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-04-24 12:16:58 +0530

    0.3

--- controllers/Rating.js
+++ controllers/Rating.js
@@ -2,10 +2,21 @@ const User = require("../models/User");
 
 exports.ranking = async (req, res) => {
   try {
+    const {id}=req.body;
     const users = await User.find({}, { email: 1, Rating: 1, _id: 1 })
       .sort({ Rating: -1 });
+   const one=await User.findOne({_id:id});
+    let rank=0;
+    users.forEach((user, index) => {
+      if (user._id == id) {
+        rank++;
+        res.status(200).json({users,rank,self:one});
+        return;
+      } else {
+        rank++;
+      }
+    });
 
-    res.status(200).json(users);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
